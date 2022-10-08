75029bbdcf32036426859c5c4d024e49b1c3ef1e
Mollie Munoz <momuno@microsoft.com>
2022-10-07 22:17:27 -0700

    Update readme and demo code

--- examples/demo_blink_serialread/demo_blink_serialread.ino
+++ examples/demo_blink_serialread/demo_blink_serialread.ino
@@ -1,26 +1,11 @@
 /*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ *
+ * This is a basic multi-threading demo using the Azure RTOS ThreadX for
+ * Arduino library. It combines the classic Blink example with serial input.
+ * This sketch is part of the Azure RTOS ThreadX tutorial series published on 
+ * Hackster.io.
+ */
 
 /* (1) Add the Azure RTOS ThreadX library header file. */
 #include <tx_api.h>
@@ -39,7 +24,6 @@ void thread_0_entry(ULONG thread_input)
 {
   (VOID)thread_input;
 
-  /* This thread simply sits in while-forever-sleep loop.  */
   while(1)
   {
     /* (5) Move the LED blink logic into the thread's entry function. */
@@ -62,8 +46,8 @@ void thread_1_entry(ULONG thread_input)
   {
     if (Serial.available() > 0)
     {
-      char byteRead = Serial.read();
-      Serial.print(byteRead);
+      char byte_read = Serial.read();
+      Serial.print(byte_read);
     }
   }
 }
